d4af788deb6c248a5ec54124a97140ba41ce4ecb
mekiro1006 <kirbyrojas1995@gmail.com>
2025-06-01 22:29:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
